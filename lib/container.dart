9fbc1a0582cbb77193297b1e03fba70354ac4d67
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-09-28 06:38:10 +0700

    Container - Add Scaffold with App Bar

--- lib/container.dart
+++ lib/container.dart
@@ -5,7 +5,8 @@ class ContainerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Container'))
+    );
   }
 }
