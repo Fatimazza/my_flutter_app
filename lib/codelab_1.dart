df8f3cc97e0ebb3791e713d3cccd6d9687896a20
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-09-30 10:31:15 +0700

    Codelab 1 - Wrap Column with SafeArea to give space based on device padding

--- lib/codelab_1.dart
+++ lib/codelab_1.dart
@@ -6,14 +6,14 @@ class Codelab1Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+        body: SafeArea(
+      child: Column(
         children: <Widget>[
           Container(
               margin: const EdgeInsets.only(top: 16.0),
-              child: const Text('Farm House Lembang')
-          )
+              child: const Text('Farm House Lembang'))
         ],
       ),
-    );
+    ));
   }
 }
