64a3b13a6eff7fba7c6699c0b66ff1ce6955eacf
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-09-30 07:47:37 +0700

    Row Column - Add Scaffold with App Bar

--- lib/row_column.dart
+++ lib/row_column.dart
@@ -5,7 +5,10 @@ class RowColumnPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Row and Column')
+      )
+    );
   }
 }
