d2929f657574fe0951ad565ba413ee59f748067a
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-02 18:16:07 +0700

    Input Widget - Fix create State for Switch

--- lib/switch.dart
+++ lib/switch.dart
@@ -4,7 +4,7 @@ class SwitchPage extends StatefulWidget {
   const SwitchPage({super.key});
 
   @override
-  State<StatefulWidget> createState() => _SwitchPageState();
+  _SwitchPageState createState() => _SwitchPageState();
 }
 
 class _SwitchPageState extends State<SwitchPage> {
