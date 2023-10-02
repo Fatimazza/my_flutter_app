4e267a604669f9f29ce43470e35dbecd25914062
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-02 17:51:03 +0700

    Input Widget - Add State Class for Switch Stateful Widget

--- lib/switch.dart
+++ lib/switch.dart
@@ -4,8 +4,14 @@ class Switch extends StatefulWidget {
   const Switch({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
+  State<StatefulWidget> createState() => _SwitchState();
+}
+
+class _SwitchState extends State<Switch> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
     throw UnimplementedError();
   }
 }
