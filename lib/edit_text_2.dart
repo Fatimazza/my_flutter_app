bb90b7bd8022df5456df29ecd438c038fbe25995
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-02 17:35:39 +0700

    Input Widget - Add new file for Edit Text with Controller page

---
+++ lib/edit_text_2.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class EditText2 extends StatefulWidget {
+  const EditText2({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
