de8128e8f5cf9e325e5692057109b56eea3290be
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-02 11:15:54 +0700

    Input Widget - Prepare Stateful and State Class for Edit Text 1

--- lib/input_widget.dart
+++ lib/input_widget.dart
@@ -12,3 +12,19 @@ class InputWidgetPage extends StatelessWidget {
         body: Container(padding: const EdgeInsets.all(16.0), child: Column()));
   }
 }
+
+class EditText1 extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class _EditTextState extends State<EditText1> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
