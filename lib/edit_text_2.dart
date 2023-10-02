2f9620f00ed973b023512414d7d96a2aa01ce9b8
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-02 17:36:31 +0700

    Input Widget - Add State Class for Edit Text with Controller

--- lib/edit_text_2.dart
+++ lib/edit_text_2.dart
@@ -4,8 +4,14 @@ class EditText2 extends StatefulWidget {
   const EditText2({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
+  State<StatefulWidget> createState() => _EditText2State();
+}
+
+class _EditText2State extends State<EditText2> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
     throw UnimplementedError();
   }
 }
