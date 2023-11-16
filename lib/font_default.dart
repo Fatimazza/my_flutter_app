19bc8f3b8869da365de5c3085245813210645450
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-16 16:47:23 +0700

    Font - Add new file for Font Default page

---
+++ lib/font_default.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FontDefaultPage extends StatelessWidget {
+  const FontDefaultPage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
