9705a80ad5642d5f8a49a807df1b6c3d3a97e257
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-16 16:23:08 +0700

    Image - Add new file for Image Network page

---
+++ lib/image_network.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ImageNetworkPage extends StatelessWidget {
+  const ImageNetworkPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
