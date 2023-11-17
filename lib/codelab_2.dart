4ce79e60cd572801addcd98abf8a11428a99071f
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-17 11:30:50 +0700

    Codelab 2 - Add Column containing Text title

--- lib/codelab_2.dart
+++ lib/codelab_2.dart
@@ -8,7 +8,16 @@ class Codelab2Page extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                  margin: const EdgeInsets.only(top: 16.0),
+                  child: const Text(
+                    'Farm House Lembang',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                  )),
+            ]
         ),
       )
     );
