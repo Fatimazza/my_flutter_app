320ba56cfbed86d3ae428dec848c57af870a5f81
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-17 11:32:23 +0700

    Codelab 2 - Add Container containing 3 Columns with Text and Icon

--- lib/codelab_2.dart
+++ lib/codelab_2.dart
@@ -17,6 +17,34 @@ class Codelab2Page extends StatelessWidget {
                     textAlign: TextAlign.center,
                     style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
                   )),
+              Container(
+                  margin: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Icon(Icons.calendar_today),
+                          SizedBox(height: 8.0),
+                          Text('Open Everyday')
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Icon(Icons.watch_later_outlined),
+                          SizedBox(height: 8.0),
+                          Text('09.00 - 20.00')
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Icon(Icons.monetization_on),
+                          SizedBox(height: 8.0),
+                          Text('Rp 25.000')
+                        ],
+                      )
+                    ],
+                  )),
             ]
         ),
       )
