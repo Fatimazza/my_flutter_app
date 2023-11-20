f3f31367e8e6b23733b22caa3e0227692851a438
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-20 13:50:55 +0700

    ListView - Add Scaffold in Dynamic ListView page

--- lib/listview_dynamic.dart
+++ lib/listview_dynamic.dart
@@ -5,7 +5,8 @@ class DynamicListViewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: ListView(),
+    );
   }
 }
