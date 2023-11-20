68a71f692b80835b2ffb1fd46210df621fd48ec4
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-20 13:58:30 +0700

    ListView -  Add list of number in Dynamic ListView page

--- lib/listview_dynamic.dart
+++ lib/listview_dynamic.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
+final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 class DynamicListViewPage extends StatelessWidget {
   const DynamicListViewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(),
-    );
+        body: ListView(
+      children: numberList.map((number) {
+        return Container();
+      }).toList(),
+    ));
   }
 }
