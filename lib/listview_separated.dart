8c20182b865c4434fe7b9540f91b3dfdfefb4915
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-20 14:13:31 +0700

    ListView - Add Scaffold in Separated ListView page

--- lib/listview_separated.dart
+++ lib/listview_separated.dart
@@ -5,7 +5,6 @@ class SeparatedListViewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(body: ListView());
   }
 }
