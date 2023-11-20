be835e7617afe5ef1ff96d0bcd0ce09df720300f
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-20 14:08:26 +0700

    ListView - Make ListView with Builder in Builder ListView page

--- lib/listview_builder.dart
+++ lib/listview_builder.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 
 class BuilderListViewPage extends StatelessWidget {
-  const BuilderListViewPage({super.key});
+  final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView(
-          
-        ));
+        body: ListView.builder(
+      itemCount: numberList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 250,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            border: Border.all(color: Colors.black),
+          ),
+          child: Center(
+            child: Text(
+              '${numberList[index]}',
+              style: const TextStyle(fontSize: 50),
+            ),
+          ),
+        );
+      },
+    ));
   }
 }
