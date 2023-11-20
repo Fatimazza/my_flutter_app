db5ee327ffbc2716cb40db4b3626677df4bde3be
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-20 13:45:02 +0700

    ListView - Add more Container as item in ListView static

--- lib/listview_static.dart
+++ lib/listview_static.dart
@@ -19,7 +19,46 @@ class StaticListViewPage extends StatelessWidget {
             style: TextStyle(fontSize: 50),
           ),
         ),
-      )
+      ),
+      Container(
+        height: 250,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          border: Border.all(color: Colors.black),
+        ),
+        child: const Center(
+          child: Text(
+            '2',
+            style: TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
+      Container(
+        height: 250,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          border: Border.all(color: Colors.black),
+        ),
+        child: const Center(
+          child: Text(
+            '3',
+            style: TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
+      Container(
+        height: 250,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          border: Border.all(color: Colors.black),
+        ),
+        child: const Center(
+          child: Text(
+            '4',
+            style: TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
     ]));
   }
 }
