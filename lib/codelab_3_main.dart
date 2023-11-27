903a26612a39c07d5a3062ce57634a7c96ed32c0
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-27 13:26:28 +0700

    Codelab 3 - Add Inkwell to contain Card on Main Page

--- lib/codelab_3_main.dart
+++ lib/codelab_3_main.dart
@@ -7,33 +7,35 @@ class Codelab3MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Wisata Bandung')),
-        body: Card(
-            child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Image.asset('images/wisata-farm-house.jpg'),
-              ),
-              Expanded(
-                  flex: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Text(
-                          'Farm House Lembang',
-                          style: const TextStyle(fontSize: 16.0),
+        body: InkWell(
+            onTap: () {},
+            child: Card(
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Image.asset('images/wisata-farm-house.jpg'),
+                  ),
+                  Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(
+                              'Farm House Lembang',
+                              style: const TextStyle(fontSize: 16.0),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text('Lembang'),
+                          ],
                         ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Text('Lembang'),
-                      ],
-                    ),
-                  ))
-            ])));
+                      ))
+                ]))));
   }
 }
