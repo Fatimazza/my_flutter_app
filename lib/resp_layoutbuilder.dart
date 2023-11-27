45bcc7e1180264557bacf4245e4a8e8aef2aa094
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-27 11:46:02 +0700

    Responsive Layout Builder - Add two Texts to display screen size

--- lib/resp_layoutbuilder.dart
+++ lib/resp_layoutbuilder.dart
@@ -5,12 +5,28 @@ class ResponsiveLayoutBuilderPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    
     return Scaffold(
         backgroundColor: Colors.deepPurple,
         body: Row(children: [
           Expanded(child: LayoutBuilder(
               builder: (BuildContext context, BoxConstraints constraints) {
-            return Column();
+            return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'MediaQuery: ${screenSize.width.toStringAsFixed(2)}',
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    'LayoutBuilder: ${constraints.maxWidth}',
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                    textAlign: TextAlign.center,
+                  )
+                ]);
           }))
         ]));
   }
