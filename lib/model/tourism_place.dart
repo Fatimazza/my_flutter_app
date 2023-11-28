02ba777e4ec9ac2ccc9e97ebccc09c6bf8d2712e
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-28 10:46:00 +0700

    Codelab 3 - Add Tourism Place data

--- lib/model/tourism_place.dart
+++ lib/model/tourism_place.dart
@@ -18,4 +18,21 @@ class TourismPlace {
     required this.imageAsset,
     required this.imageUrls,
   });
+
+  var tourismPlaceList = [
+    TourismPlace(
+        name: 'Farm House Lembang',
+        location: 'Lembang',
+        description:
+            'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+        openDays: 'Open Everyday',
+        openTime: '09:00 - 20:00',
+        ticketPrice: 'Rp 25000',
+        imageAsset: 'images/farm-house.jpg',
+        imageUrls: [
+          'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg',
+          'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg',
+          'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'
+        ])
+  ];
 }
