39e9de8ae14d4c25f2adf334231a617be3aeff86
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-29 13:18:14 +0700

    Codelab 4 - Add List of Tourism Place in Main Screen

--- lib/codelab_4_main.dart
+++ lib/codelab_4_main.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/model/tourism_place.dart';
 
 class Codelab4MainScreen extends StatelessWidget {
-  const Codelab4MainScreen({super.key});
-  
+  const Codelab4MainScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Wisata Bandung')));
+    return Scaffold(
+        appBar: AppBar(title: const Text('Wisata Bandung')),
+        body: ListView.builder(
+            itemBuilder: (context, index) {
+              final TourismPlace place = tourismPlaceList[index];
+              return Card(
+                  child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Image.asset(place.imageAsset),
+                    ),
+                    Expanded(
+                        flex: 2,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Text(
+                                place.name,
+                                style: const TextStyle(fontSize: 16.0),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(place.location),
+                            ],
+                          ),
+                        ))
+                  ]));
+            },
+            itemCount: tourismPlaceList.length));
   }
 }
