c4de00e4afa670f7c27038540ebec5a33f6cdbdb
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-29 13:41:05 +0700

    Codelab 4 - Show Tourism Place as Grid View in Main Screen

--- lib/codelab_4_main.dart
+++ lib/codelab_4_main.dart
@@ -24,9 +24,46 @@ class TourismPlaceGrid extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(24.0),
       child: GridView.count(
-        crossAxisCount: 4,
-        children: [],
-      ),
+          crossAxisCount: 4,
+          children: tourismPlaceList.map((place) {
+            return InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Codelab4DetailScreen(place: place);
+                }));
+              },
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        place.imageAsset,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(
+                        place.name,
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                      child: Text(
+                        place.location,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }).toList()),
     );
   }
 }
