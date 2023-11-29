7f4cf59776a933c7907f5075d522025091203da4
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-29 11:32:03 +0700

    Codelab 4 - Show Main Screen with Material App and Scaffold

--- lib/codelab_4.dart
+++ lib/codelab_4.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/codelab_4_main.dart';
 
 class Codelab4Page extends StatelessWidget {
   const Codelab4Page({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: const Codelab4MainScreen(),
+    );
   }
 }
--- lib/codelab_4_main.dart
+++ lib/codelab_4_main.dart
@@ -5,7 +5,6 @@ class Codelab4MainScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(appBar: AppBar(title: const Text('Wisata Bandung')));
   }
 }
