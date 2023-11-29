db0e194f18a1cbd7936ddd34741804e45f619621
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-29 11:28:39 +0700

    Codelab 4 - Add Codelab 4 Main Screen

---
+++ lib/codelab_4_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Codelab4MainScreen extends StatelessWidget {
+  const Codelab4MainScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
