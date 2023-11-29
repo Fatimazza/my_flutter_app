a5fe1b11f085a2a0189b5d42133122b093abcd3f
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-29 13:25:16 +0700

    Codelab 4 - Add Favorite Button class in Detail Screen

--- lib/codelab_4_detail.dart
+++ lib/codelab_4_detail.dart
@@ -3,10 +3,37 @@ import 'package:my_flutter_app/model/tourism_place.dart';
 
 class Codelab4DetailScreen extends StatelessWidget {
   final TourismPlace place;
+
   const Codelab4DetailScreen({Key? key, required this.place}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: SingleChildScrollView(child: Column()));
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({Key? key}) : super(key: key);
+
+  @override
+  _FavoriteButtonState createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        isFavorite ? Icons.favorite : Icons.favorite_border,
+        color: Colors.red,
+      ),
+      onPressed: () {
+        setState(() {
+          isFavorite = !isFavorite;
+        });
+      },
+    );
   }
 }
