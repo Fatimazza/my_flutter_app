d6efa441f05b2ace45d4fd3e0377c8a97cf4519d
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-29 14:08:40 +0700

    Codelab 4 - Add same code for Detail Web Page in Detail Screen

--- lib/codelab_4_detail.dart
+++ lib/codelab_4_detail.dart
@@ -121,12 +121,108 @@ class DetailMobilePage extends StatelessWidget {
 }
 
 class DetailWebPage extends StatelessWidget {
-  const DetailWebPage({super.key});
+  final TourismPlace place;
+
+  const DetailWebPage({super.key, required this.place});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+          Stack(children: <Widget>[
+            Image.asset(place.imageAsset),
+            SafeArea(
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                            backgroundColor: Colors.white,
+                            child: IconButton(
+                                icon: const Icon(
+                                  Icons.arrow_back,
+                                  color: Colors.black,
+                                ),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                })),
+                        const FavoriteButton(),
+                      ],
+                    )))
+          ]),
+          Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              child: Text(
+                place.name,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: 'Staatliches',
+                ),
+              )),
+          Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.calendar_today),
+                      SizedBox(height: 8.0),
+                      Text(
+                        place.openDays,
+                        style: informationTextStyle,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.watch_later_outlined),
+                      SizedBox(height: 8.0),
+                      Text(
+                        place.openTime,
+                        style: informationTextStyle,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.monetization_on),
+                      SizedBox(height: 8.0),
+                      Text(
+                        place.ticketPrice,
+                        style: informationTextStyle,
+                      )
+                    ],
+                  )
+                ],
+              )),
+          Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                place.description,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0, fontFamily: 'Oxygen'),
+              )),
+          SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: place.imageUrls.map((url) {
+                  return Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(url),
+                    ),
+                  );
+                }).toList(),
+              ))
+        ])));
   }
 }
 
