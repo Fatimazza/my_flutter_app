e7b43a0503446424a278ed194130465dc1f376d9
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-29 13:23:23 +0700

    Codelab 4 - Open Detail Page when tapping on Card in Main Screen

--- lib/codelab_4_detail.dart
+++ lib/codelab_4_detail.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/model/tourism_place.dart';
 
 class Codelab4DetailScreen extends StatelessWidget {
-  const Codelab4DetailScreen({super.key});
+  final TourismPlace place;
+  const Codelab4DetailScreen({Key? key, required this.place}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(body: SingleChildScrollView(child: Column()));
   }
 }
--- lib/codelab_4_main.dart
+++ lib/codelab_4_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/codelab_4_detail.dart';
 import 'package:my_flutter_app/model/tourism_place.dart';
 
 class Codelab4MainScreen extends StatelessWidget {
@@ -11,34 +12,41 @@ class Codelab4MainScreen extends StatelessWidget {
         body: ListView.builder(
             itemBuilder: (context, index) {
               final TourismPlace place = tourismPlaceList[index];
-              return Card(
-                  child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Image.asset(place.imageAsset),
-                    ),
-                    Expanded(
-                        flex: 2,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              Text(
-                                place.name,
-                                style: const TextStyle(fontSize: 16.0),
+              return InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return Codelab4DetailScreen(place: place);
+                    }));
+                  },
+                  child: Card(
+                      child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                        Expanded(
+                          flex: 1,
+                          child: Image.asset(place.imageAsset),
+                        ),
+                        Expanded(
+                            flex: 2,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Text(
+                                    place.name,
+                                    style: const TextStyle(fontSize: 16.0),
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text(place.location),
+                                ],
                               ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Text(place.location),
-                            ],
-                          ),
-                        ))
-                  ]));
+                            ))
+                      ])));
             },
             itemCount: tourismPlaceList.length));
   }
