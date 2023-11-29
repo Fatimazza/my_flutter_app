21d670010aef35fdb087d62cc0db7a058927e3cd
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-29 13:27:02 +0700

    Codelab 4 - Display Tourism Place data in Detail Screen

--- lib/codelab_4_detail.dart
+++ lib/codelab_4_detail.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_flutter_app/model/tourism_place.dart';
 
+var informationTextStyle = const TextStyle(fontFamily: 'Oxygen');
+
 class Codelab4DetailScreen extends StatelessWidget {
   final TourismPlace place;
 
@@ -8,7 +10,102 @@ class Codelab4DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SingleChildScrollView(child: Column()));
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+          Stack(children: <Widget>[
+            Image.asset(place.imageAsset),
+            SafeArea(
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                            backgroundColor: Colors.white,
+                            child: IconButton(
+                                icon: const Icon(
+                                  Icons.arrow_back,
+                                  color: Colors.black,
+                                ),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                })),
+                        const FavoriteButton(),
+                      ],
+                    )))
+          ]),
+          Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              child: Text(
+                place.name,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: 'Staatliches',
+                ),
+              )),
+          Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.calendar_today),
+                      SizedBox(height: 8.0),
+                      Text(
+                        place.openDays,
+                        style: informationTextStyle,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.watch_later_outlined),
+                      SizedBox(height: 8.0),
+                      Text(
+                        place.openTime,
+                        style: informationTextStyle,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.monetization_on),
+                      SizedBox(height: 8.0),
+                      Text(
+                        place.ticketPrice,
+                        style: informationTextStyle,
+                      )
+                    ],
+                  )
+                ],
+              )),
+          Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                place.description,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0, fontFamily: 'Oxygen'),
+              )),
+          SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: place.imageUrls.map((url) {
+                  return Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(url),
+                    ),
+                  );
+                }).toList(),
+              ))
+        ])));
   }
 }
 
